60e6e70355bba9a46c636760a76f7ed04131dfc6
SSCS148 <sacha.02.cohen.04@gmail.com>
2024-03-03 14:26:38 +0200

    add

---
+++ Week 2/Day 1/XP.py
@@ -0,0 +1,57 @@
+#Exersice 1:
+# print("Hello world "*4)
+
+#Exersice 2:
+# print((99**3)*8)
+
+#Exersice 3:
+# false
+# true
+#false
+#Error
+#false
+
+#Exersice 4:
+# computer_brand = "apple"
+# print("I have a",computer_brand,"computer")
+
+#Exersice 5:
+# name = 'Sacha'
+# age = 23
+# shoe_size = 41
+# info = f"My name is {name}, i'am {age} years old and my shoe\'s size is {shoe_size}."
+# print(info)
+
+
+#Exersice 6:
+# num_1 = 6
+# num_2 = 4
+# if num_1 > num_2:
+#     print("Hello World")
+
+
+#Exersice 7:
+# num = int(input("Enter a number : "))
+# if num % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+
+#Exersice 8:
+# My_name = "Sacha"
+# Your_name = input("What's your name ?")
+# if My_name == Your_name:
+#     print("Amazing we have the same name")
+# else:
+#     print("FAERGAZETGRZTBTVYRH  SCABABOUM")
+
+
+#Exersice 9:
+# height = int(input('What is your height in inches?\n'))
+# cm = (height*2.54)
+# if cm > 145:
+#     print("You are old enough to ride")
+# else:
+#     print("You need to grow some more to ride")
+
