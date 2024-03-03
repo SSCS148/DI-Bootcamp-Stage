cbf964cc81fbb1ce75c2e092ef4bb4a953cbc924
SSCS148 <sacha.02.cohen.04@gmail.com>
2024-03-03 15:46:32 +0200

    add

---
+++ Week 2/Daily_challenge.py
@@ -0,0 +1,19 @@
+# #Challenge
+
+# sentence = str(input ('Insert a 10 characters sentence \n'))
+# print(sentence)
+# if len(sentence) < 10:
+#     print("string not long enough")
+# elif len(sentence) > 10:
+#     print("string too long")
+# elif len(sentence) == 10:
+#     print("perfect string")
+    
+# first = sentence[0]
+# last = sentence[-1]
+# print(f'{first}{last}')
+
+# space = ''
+# for i in sentence:
+#     space = space +i
+#     print(space)
