89c45218c55cea1e5496ae55f53bfade93a0c1bd
SSCS148 <sacha.02.cohen.04@gmail.com>
2024-03-04 15:42:08 +0200

    add

---
+++ Week 2/Day 2/Xp.py
@@ -0,0 +1,33 @@
+# #Exercise 1
+
+# my_fav_numbers = {3, 7, 21, 42}
+# my_fav_numbers.add(8)
+# my_fav_numbers.add(13)
+# my_fav_numbers.remove(13)
+# friend_fav_numbers = {5, 11, 19, 23}
+# our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
+# print(my_fav_numbers)
+# print(friend_fav_numbers)
+# print(our_fav_numbers)
+
+# #Exercise 2
+# NO
+
+# Exercise 3
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# basket.pop(0)
+# basket.pop(2)
+# basket.append('Kiwi')
+# basket.insert(0,"Apple")
+# appple_count = basket.count("Apple")
+# basket.clear()
+# print(basket)
+
+
+# Exercise 4
+# A float is a number that contains a decimal point or is written using scientific notation. ex: 1,-1,
+# An integer is a whole number, either positive or negative, without any decimal point. ex: 1,-1,1.23,-1.23
+list = [1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+
+
+
