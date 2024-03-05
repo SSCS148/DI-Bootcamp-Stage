6e66d800e25e0b50278f0bd39047f7e6ec5320fa
SSCS148 <sacha.02.cohen.04@gmail.com>
2024-03-05 08:33:53 +0200

    add

---
+++ Week 2/Day 2/Daily_challenge.py
@@ -0,0 +1,22 @@
+# # Challenge 1
+
+# number = int(input("Enter a number: "))
+# length = int(input("Enter the length of the list: "))
+
+# multiples = []
+
+# for i in range(1, length + 1):
+#     multiples.append(number * i)
+
+# print("List of multiples of", number, "until length", length, ":")
+# print(multiples)
+
+# challenge 2
+# input_string = input("Enter a string: ")
+# new_string = ""
+
+# for i in range(len(input_string)):
+#     if i == 0 or input_string[i] != input_string[i - 1]:
+#         new_string += input_string[i]
+
+# print("New string with duplicate consecutive letters removed:", new_string)
