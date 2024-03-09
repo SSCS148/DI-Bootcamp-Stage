4e27a006b7bb5050322f681e51ba0c09904dbcbb
SSCS148 <sacha.02.cohen.04@gmail.com>
2024-03-09 23:32:19 +0200

    add

--- Week 2/Day 3/cours.py
+++
@@ -1,8 +0,0 @@
-fruits = input("Enter a list of fruits: ")
-storage = fruits.split()
-print(fruits)
-name_of_fruit = input("Enter a name of fruit: ")
-if name_of_fruit in storage:
-    print(name_of_fruit + " is already in the list.")
-else:
-    print(name_of_fruit + " is not in the list.")
