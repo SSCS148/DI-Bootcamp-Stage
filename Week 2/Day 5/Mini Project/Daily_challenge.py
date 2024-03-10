16f2285022342785772a51f4724b5231ff15fe67
SSCS148 <sacha.02.cohen.04@gmail.com>
2024-03-10 08:55:05 +0200

    add

--- Week 2/Day 5/Mini Project/Daily_challenge.py
+++ Week 2/Day 5/Mini Project/Daily_challenge.py
@@ -1,17 +1,17 @@
-# Exercise 1
+# # Exercise 1
 
-def sort_words(input_str):
-    words = [word.strip() for word in input_str.split(',')]
+# def sort_words(input_str):
+#     words = [word.strip() for word in input_str.split(',')]
     
-    sorted_words = sorted(words)
+#     sorted_words = sorted(words)
     
-    result = ','.join(sorted_words)
+#     result = ','.join(sorted_words)
     
-    return result
+#     return result
 
-input_str = input("Enter a comma-separated sequence of words: ")
-sorted_sequence = sort_words(input_str)
-print("Sorted sequence:", sorted_sequence)
+# input_str = input("Enter a comma-separated sequence of words: ")
+# sorted_sequence = sort_words(input_str)
+# print("Sorted sequence:", sorted_sequence)
 
 
 # Exercise 2
