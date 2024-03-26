08e4f755a737e37017f11a0c1b6356fef3acc97b
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-03-27 01:47:57 +0200

    add

---
+++ Week 4/Day 1/DailyChallenge_Stars.js
@@ -0,0 +1,22 @@
+// Using One loop
+// const rows = 6;
+
+// console.log("Pattern using one loop:");
+// let pattern = '';
+// for (let i = 1; i <= rows; i++) {
+//     pattern += '* ';
+//     console.log(pattern);
+// }
+
+
+// const rows = 6;
+// // Using two loop
+// console.log("Pattern using one loop:");
+// for (let i = 1; i <= rows; i++) {
+//     let pattern = '';
+//     for (let j = 1; j <= i; j++) {
+//         pattern += '* ';
+//     }
+//     console.log(pattern);
+// }
+
---
+++ Week 4/Day 1/Daily_challenge.js
@@ -0,0 +1,16 @@
+// // Create a variable called sentence
+// const sentence = "This dinner is not that bad ! You cook well";
+
+// // Find the position of the substring "not" in the sentence
+// const wordNot = sentence.indexOf("not");
+
+// // Find the position of the substring "bad" in the sentence
+// const wordBad = sentence.indexOf("bad");
+
+// // Check if "bad" comes after "not" and replace the substring "not...bad" with "good" if true
+// if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+//     const modifiedSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+//     console.log("The result is:", modifiedSentence);
+// } else {
+//     console.log("The result is:", sentence);
+// }
