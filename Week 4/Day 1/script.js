6ce8789ca06af4bed52dfd5e2e84caca44c7c08c
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-03-27 01:40:06 +0200

    add

---
+++ Week 4/Day 1/script.js
@@ -0,0 +1,89 @@
+// let name = value
+// let name2;
+// name2 = 5
+
+// let str = 'test'
+// let num = 5
+// let bool = 4 > 5
+// let void = null
+// let un = undefined
+// let arr : []
+// let obj = {}
+
+// let greeting = 'hello'
+// let name3 = 'Nadav'
+// let fullGreeting = greeting + name3
+// console.log(fullGreeting)
+// console.log(greeting,name3);
+// let lonString = "LOremloremloremloremloremlor\
+// loremloremloremloRemloremloremloremlorem\
+// loremloremloremloremloremloremloremlorem";
+
+// console.log(lonString.length);
+// console.log(lonString.split(''));
+// console.log(lonString.substring(0,20));
+// let small = lonString.toLowerCase()
+
+// let compare = 13 == 12
+
+// let str = "Hello Everyone, please say hello to the class";
+// let posUppercaseHello = str.indexOf("Hello"); // case sensitive
+// let posLowercaseHello = str.indexOf("hello");
+// console.log(posUppercaseHello) // 0
+// console.log(posLowercaseHello) // 27
+
+// let secondstr = "hello Everyone, please say hello to the class";
+// let secondStrPosHello = secondstr.indexOf("hello"); 
+// console.log(secondStrPosHello) // 0
+
+// 1. Create these variables and give them values:
+
+// addressNumber
+// addressStreet
+// country
+// 2. Write a variable named globalAddress, and concatenate inside, the variables:
+
+// addressNumber
+// addressStreet
+// country
+// In order to create a sentence
+// 3. Display globalAddress Example: globalAddress should display « I live in BenYehuda 5, in Israel »
+
+// let addressNumber = 202
+// let addressStreet = 'Yaffo'
+// let country = 'Israel'
+
+//  let globalAddress = `I live in ${addressStreet} ${addressNumber}, in ${country}`
+// console.log(globalAddress)
+// 1. Store your birth year in a variable.
+
+// 2. Store a future year in a variable.
+
+// 3. Calculate your possible ages for that year based on the stored values.
+
+// 4. Display : "I will be NN in YYYY", substituting the values.
+
+// let birth = 1999
+// let future = 2032
+// let calAge = `I will be ${future - birth} in ${future}`
+// console.log(calAge)
+
+// alert ('hello bro')
+// confirm('hi')
+// prompt('are you ok' )
+// let isBoss = confirm("Are you the boss?");
+// alert(isBoss); // true if OK is pressed
+
+// let sampleArray = [
+//     [1, 2, 5],
+//     [7, 6, 10, 11, 12],
+//     [3]
+// ]
+// console.log(sampleArray[2][0])
+// sampleArray[2][0] = 0
+// console.log(sampleArray)
+// console.log(sampleArray[2].length)
+
+// let colors = ["blue", "yellow", "green" ]; 
+// colors.splice(0, 1, 45, 23); 
+// console.log(colors) 
