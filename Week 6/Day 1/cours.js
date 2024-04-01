fc234af3f56a40f6cb6ffc7fac7e89efc01ccc39
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-04-01 14:05:03 +0300

    add

---
+++ Week 6/Day 1/cours.js
@@ -0,0 +1,24 @@
+// function sum (a,b){
+//     return calc = a + b;
+// }
+
+// console.log(sum(1,1))
+
+
+// function sum (a,b){
+//     return  (a + b);
+//     return (a * b);
+// }
+// let ab = (1,1);
+// let bc = (1,600);
+// console.log(ab,bc);
+
+let a = x();
+console.log(a)
+
+function x (){
+    function y(){
+        return 5;
+    }
+    return y;
+}
