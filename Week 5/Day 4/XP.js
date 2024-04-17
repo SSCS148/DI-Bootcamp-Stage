701db01ea8642f67576e4b6d80e1d0e224a55786
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-04-17 18:10:24 +0300

    add

--- Week 5/Day 4/XP.js
+++
@@ -1,5 +0,0 @@
-let usernanme;
-document.getElementById("mySubmit").onclick = function (){
-    usernanme = document.getElementById("myText").value;
-    document.getElementById("Myh1").textContent = `Hello my name is ${usernanme} `
-}
