c29ef74ca1510f7c2f133ccc94adec357e74d0bb
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-04-17 13:02:32 +0300

    add

--- Week 2/Day 4/Daily_challenge.py
+++ Week 2/Day 4/Daily_challenge.py
@@ -19,3 +19,53 @@
 # no_digits_string = "".join(no_digits_list)
 
 # print(re.sub('[^A-Za-z0-9]+', ' ', no_digits_string))
+
+
+# matrix_string = '''7ii
+# Tsx
+# h%?
+# i #
+# sM 
+# $a 
+# #t%
+# ^r!'''
+# COLUMN = 3
+# ROWS = 8
+
+# # matrix 2D list
+# # [[],[],[],[]]
+# rows = matrix_string.split('\n')
+# # print(rows)
+
+# # for i in range(len(matrix_string)):
+# #     sublist = list(matrix_string[i:i+COLUMN])
+# #     matrix.append(sublist)
+
+# # for row in rows:
+# #     matrix.append(list(row))
+
+# matrix = [list(row) for row in rows]
+
+# transposed_matrix = list(zip(*matrix))
+# print(transposed_matrix)
+
+# print(matrix)
+
+# transposed_message = [''.join(char) for char in transposed_matrix]
+# print(transposed_message)
+# dec_message = ''
+# final_str = dec_message.join(transposed_message)
+# print(final_str)
+
+# decrypted_message = ''
+# for char in final_str:
+#     if char.isalpha():
+#         decrypted_message += char
+#     else:
+#         decrypted_message += ' '
+
+# print(decrypted_message)
+
+# dec_message = ' '.join(decrypted_message.split())
+
+# print(dec_message)
