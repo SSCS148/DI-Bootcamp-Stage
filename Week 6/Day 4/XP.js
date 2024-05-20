f7e39917a21eac72bd103293f49d8bc7bf034639
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-05-20 13:49:36 +0300

    add

---
+++ Week 6/Day 4/XP.js
@@ -0,0 +1,2 @@
+const prettyJSON = JSON.stringify(marioGame, null, 2);
+console.log(prettyJSON);
---
+++ Week 7/Day 2/XP.js
@@ -0,0 +1,15 @@
+const giphyURL = "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+
+fetch(giphyURL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
