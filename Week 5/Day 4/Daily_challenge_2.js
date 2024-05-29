b3b44e9134384d848448286327353577705fad69
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-05-29 18:36:17 +0300

    add

--- Week 5/Day 4/Daily_challenge_2.js
+++ Week 5/Day 4/Daily_challenge_2.js
@@ -36,17 +36,17 @@
   
 //   Exercice 2 
 
-//   function myMove() {
-//     var elem = document.getElementById("animate");   
-//     var pos = 0;
-//     var id = setInterval(frame, 1);
-//     function frame() {
-//       if (pos == 350) {
-//         clearInterval(id);
-//       } else {
-//         pos++; 
-//         elem.style.left = pos + 'px'; 
-//       }
-//     }
-//   }
+  function myMove() {
+    var elem = document.getElementById("animate");   
+    var pos = 0;
+    var id = setInterval(frame, 1);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.left = pos + 'px'; 
+      }
+    }
+  }
   
